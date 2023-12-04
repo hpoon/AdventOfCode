617942b82570672e5a59dd14957214b16ed33159
hp <5841557+hpoon@users.noreply.github.com>
2023-12-04 00:32:05 -0500

    Completed problem 2023 day 4

--- src/main/java/com/aoc/y2023/ProblemDay4.java
+++ src/main/java/com/aoc/y2023/ProblemDay4.java
@@ -1,17 +1,73 @@
 package com.aoc.y2023;
 
 import com.aoc.ProblemDay;
+import com.google.common.collect.Sets;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ProblemDay4 extends ProblemDay<Integer, Integer> {
 
     @Override
     public Integer solveA() {
-        return 0;
+        int score = 0;
+        while (scanner.hasNextLine()) {
+            String[] line = scanner.nextLine().split(" \\| ");
+            Set<Integer> winning = Arrays.stream(line[0].split(": ")[1].split(" "))
+                    .filter(str -> !str.equals(""))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toSet());
+            Set<Integer> card = Arrays.stream(line[1].split(" "))
+                    .filter(str -> !str.equals(""))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toSet());
+            Set<Integer> matches = Sets.intersection(winning, card);
+            score += Math.pow(2, matches.size() - 1);
+        }
+        return score;
     }
 
     @Override
     public Integer solveB() {
-        return 0;
+        Map<Integer, Pair<Set<Integer>, Set<Integer>>> cardNumToSets = new HashMap<>();
+        Map<Integer, Integer> occurrences = new HashMap<>();
+        while (scanner.hasNextLine()) {
+            String[] line = scanner.nextLine().split(" \\| ");
+            Set<Integer> winning = Arrays.stream(line[0].split(": ")[1].split(" "))
+                    .filter(str -> !str.equals(""))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toSet());
+            Set<Integer> card = Arrays.stream(line[1].split(" "))
+                    .filter(str -> !str.equals(""))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toSet());
+            cardNumToSets.put(
+                    Integer.parseInt(line[0]
+                            .split(": ")[0]
+                            .replace(" ", "")
+                            .replace("Card", "")),
+                    ImmutablePair.of(winning, card));
+        }
+        Queue<Integer> queue = IntStream.rangeClosed(1, cardNumToSets.size())
+                .boxed()
+                .collect(Collectors.toCollection(LinkedList::new));
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            Set<Integer> winning = cardNumToSets.get(num).getKey();
+            Set<Integer> card = cardNumToSets.get(num).getValue();
+            Set<Integer> matches = Sets.intersection(winning, card);
+            queue.addAll(IntStream.rangeClosed(num + 1, num + matches.size())
+                    .boxed()
+                    .collect(Collectors.toList()));
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+        return occurrences.values()
+                .stream()
+                .reduce(Integer::sum)
+                .orElseThrow(() -> new RuntimeException("Shouldn't happen"));
     }
 
 }
