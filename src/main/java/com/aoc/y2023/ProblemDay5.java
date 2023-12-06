206fe10bd215b19269c8b899c94aaab11bb04fb0
hp <5841557+hpoon@users.noreply.github.com>
2023-12-06 00:00:36 -0500

    Completed problem 2023 day 5

--- src/main/java/com/aoc/y2023/ProblemDay5.java
+++ src/main/java/com/aoc/y2023/ProblemDay5.java
@@ -1,17 +1,206 @@
 package com.aoc.y2023;
 
 import com.aoc.ProblemDay;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Range;
+import com.google.common.collect.RangeSet;
+import com.google.common.collect.TreeRangeSet;
+import org.apache.commons.lang3.StringUtils;
 
-public class ProblemDay5 extends ProblemDay<Integer, Integer> {
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ProblemDay5 extends ProblemDay<Long, Long> {
 
     @Override
-    public Integer solveA() {
-        return 0;
+    public Long solveA() {
+        List<Long> seeds = seedsA();
+        Map<Range<Long>, Range<Long>> seedToSoil = range("seed-to-soil map:");
+        Map<Range<Long>, Range<Long>> soilToFertilizer = range("soil-to-fertilizer map:");
+        Map<Range<Long>, Range<Long>> fertilizerToWater = range("fertilizer-to-water map:");
+        Map<Range<Long>, Range<Long>> waterToLight = range("water-to-light map:");
+        Map<Range<Long>, Range<Long>> lightToTemperature = range("light-to-temperature map:");
+        Map<Range<Long>, Range<Long>> temperatureToHumidity = range("temperature-to-humidity map:");
+        Map<Range<Long>, Range<Long>> humidityToLocation = range("humidity-to-location map:");
+
+        long min = Long.MAX_VALUE;
+        for (long seed : seeds) {
+            long soil = getA(seedToSoil, seed);
+            long fertilizer = getA(soilToFertilizer, soil);
+            long water = getA(fertilizerToWater, fertilizer);
+            long light = getA(waterToLight, water);
+            long temperature = getA(lightToTemperature, light);
+            long humidity = getA(temperatureToHumidity, temperature);
+            long location = getA(humidityToLocation, humidity);
+            if (location < min) {
+                min = location;
+            }
+        }
+        return min;
     }
 
     @Override
-    public Integer solveB() {
-        return 0;
+    public Long solveB() {
+        RangeSet<Long> seeds = seedsB();
+        Map<Range<Long>, Range<Long>> seedToSoil = range("seed-to-soil map:");
+        Map<Range<Long>, Range<Long>> soilToFertilizer = range("soil-to-fertilizer map:");
+        Map<Range<Long>, Range<Long>> fertilizerToWater = range("fertilizer-to-water map:");
+        Map<Range<Long>, Range<Long>> waterToLight = range("water-to-light map:");
+        Map<Range<Long>, Range<Long>> lightToTemperature = range("light-to-temperature map:");
+        Map<Range<Long>, Range<Long>> temperatureToHumidity = range("temperature-to-humidity map:");
+        Map<Range<Long>, Range<Long>> humidityToLocation = range("humidity-to-location map:");
+        return solveB(
+                seeds,
+                seedToSoil,
+                soilToFertilizer,
+                fertilizerToWater,
+                waterToLight,
+                lightToTemperature,
+                temperatureToHumidity,
+                humidityToLocation);
+    }
+
+    @VisibleForTesting
+    protected Long solveB(RangeSet<Long> seeds, Map<Range<Long>, Range<Long>> seedToSoil, Map<Range<Long>, Range<Long>> soilToFertilizer, Map<Range<Long>, Range<Long>> fertilizerToWater, Map<Range<Long>, Range<Long>> waterToLight, Map<Range<Long>, Range<Long>> lightToTemperature, Map<Range<Long>, Range<Long>> temperatureToHumidity, Map<Range<Long>, Range<Long>> humidityToLocation) {
+        RangeSet<Long> soils = getB(seeds, seedToSoil);
+        RangeSet<Long> fertilizers = getB(soils, soilToFertilizer);
+        RangeSet<Long> water = getB(fertilizers, fertilizerToWater);
+        RangeSet<Long> light = getB(water, waterToLight);
+        RangeSet<Long> temperature = getB(light, lightToTemperature);
+        RangeSet<Long> humidity = getB(temperature, temperatureToHumidity);
+        RangeSet<Long> location = getB(humidity, humidityToLocation);
+        return location.span().lowerEndpoint();
+    }
+
+    @VisibleForTesting
+    protected RangeSet<Long> getB(RangeSet<Long> srcSet,
+                                  Map<Range<Long>, Range<Long>> dstMap) {
+        RangeSet<Long> result = TreeRangeSet.create();
+        Queue<Range<Long>> queue = new LinkedList<>();
+        queue.addAll(srcSet.asRanges());
+        while (!queue.isEmpty()) {
+            Range<Long> src = queue.poll();
+            boolean hasAnyConnection = dstMap.keySet()
+                    .stream()
+                    .filter(srcKey -> srcKey.isConnected(src))
+                    .anyMatch(srcKey -> !srcKey.intersection(src).isEmpty());
+            if (!hasAnyConnection) {
+                result.add(src);
+                continue;
+            }
+            for (Range<Long> srcKey : dstMap.keySet()) {
+                if (srcKey.encloses(src)) {
+                    Range<Long> dst = dstMap.get(srcKey);
+                    long diff = dst.lowerEndpoint() - srcKey.lowerEndpoint();
+                    result.add(Range.closedOpen(src.lowerEndpoint() + diff, src.upperEndpoint() + diff));
+                    break;
+                } else if (src.encloses(srcKey)) {
+                    Range<Long> dst = dstMap.get(srcKey);
+                    result.add(dst);
+                    queue.add(Range.closedOpen(src.lowerEndpoint(), srcKey.lowerEndpoint()));
+                    queue.add(Range.closedOpen(srcKey.upperEndpoint(), src.upperEndpoint()));
+                    break;
+                } else {
+                    if (!srcKey.isConnected(src)) {
+                        continue;
+                    }
+                    Range<Long> intersection = src.intersection(srcKey);
+                    if (intersection.isEmpty()) {
+                        continue;
+                    }
+                    Range<Long> dst = dstMap.get(srcKey);
+                    long diff = dst.lowerEndpoint() - srcKey.lowerEndpoint();
+                    // Left offset
+                    if (src.lowerEndpoint() < srcKey.lowerEndpoint() && src.upperEndpoint() < srcKey.upperEndpoint()) {
+                        result.add(Range.closedOpen(
+                                intersection.lowerEndpoint() + diff,
+                                src.upperEndpoint() + diff));
+                        queue.add(Range.closedOpen(src.lowerEndpoint(), srcKey.lowerEndpoint()));
+                    // Right offset
+                    } else if (src.lowerEndpoint() > srcKey.lowerEndpoint() && src.upperEndpoint() > srcKey.upperEndpoint()) {
+                        result.add(Range.closedOpen(
+                                src.lowerEndpoint() + diff,
+                                intersection.upperEndpoint() + diff));
+                        queue.add(Range.closedOpen(srcKey.upperEndpoint(), src.upperEndpoint()));
+                    } else {
+                        if (src.upperEndpoint() < srcKey.upperEndpoint()) {
+                            continue;
+                        } else if (src.upperEndpoint() > srcKey.upperEndpoint()) {
+                            result.add(Range.closedOpen(srcKey.upperEndpoint(), src.upperEndpoint()));
+                        } else {
+                            continue;
+                        }
+                    }
+//                    result.asRanges().stream().filter(r -> r.lowerEndpoint() == 0).findAny().isPresent()
+                }
+            }
+        }
+        return result;
+    }
+
+    long getA(Map<Range<Long>, Range<Long>> map, long key) {
+        for (Range<Long> range : map.keySet()) {
+            if (range.contains(key)) {
+                long diff = key - range.lowerEndpoint();
+                return map.get(range).lowerEndpoint() + diff;
+            }
+        }
+        return key;
+    }
+
+    List<Long> seedsA() {
+        while (scanner.hasNextLine()) {
+            String[] tokens = scanner.nextLine().split(" ");
+            if (tokens[0].equals("seeds:")) {
+                return IntStream.range(1, tokens.length)
+                        .mapToObj(i -> Long.parseLong(tokens[i]))
+                        .collect(Collectors.toList());
+            }
+        }
+        throw new RuntimeException("Shouldn't happen");
+    }
+
+    RangeSet<Long> seedsB() {
+        RangeSet<Long> seeds = TreeRangeSet.create();
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            if (StringUtils.isBlank(str)) {
+                break;
+            }
+            String[] tokens = str.split(" ");
+            if (tokens[0].equals("seeds:")) {
+                for (int i = 1; i < tokens.length; i += 2) {
+                    long start = Long.parseLong(tokens[i]);
+                    seeds.add(Range.closedOpen(start, start + Long.parseLong(tokens[i+1])));
+                }
+            }
+        }
+        return seeds;
+    }
+
+    Map<Range<Long>, Range<Long>> range(String token) {
+        Map<Range<Long>, Range<Long>> map = new HashMap<>();
+        boolean started = false;
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            if (StringUtils.isBlank(str)) {
+                if (started) {
+                    break;
+                }
+                continue;
+            }
+            if (str.equals(token)) {
+                started = true;
+                continue;
+            }
+            String[] tokens = str.split(" ");
+            long range = Long.parseLong(tokens[2]);
+            long src = Long.parseLong(tokens[1]);
+            long dest = Long.parseLong(tokens[0]);
+            map.put(Range.closedOpen(src, src + range), Range.closedOpen(dest, dest + range));
+        }
+        return map;
     }
 
 }
--- src/main/test/java/com/aoc/y2023/ProblemDay5Test.java
+++ src/main/test/java/com/aoc/y2023/ProblemDay5Test.java
@@ -1,8 +1,15 @@
 package com.aoc.y2023;
 
 import com.aoc.ProblemDayTest;
+import com.google.common.collect.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -18,13 +25,141 @@ public class ProblemDay5Test implements ProblemDayTest {
     @Test
     @Override
     public void testSolveA() {
-        assertThat(problem.solveA()).isEqualTo(0);
+        assertThat(problem.solveA()).isEqualTo(1181555926);
     }
 
     @Test
     @Override
     public void testSolveB() {
-        assertThat(problem.solveB()).isEqualTo(0);
+        assertThat(problem.solveB()).isEqualTo(37806486);
+    }
+
+    @Test
+    public void testSolveB_simple() {
+        RangeSet<Long> seeds = seeds("79 14 55 13");
+        Map<Range<Long>, Range<Long>> seedToSoil = range(ImmutableList.of(
+                "50 98 2", "52 50 48"));
+        Map<Range<Long>, Range<Long>> soilToFertilizer = range(ImmutableList.of(
+                "0 15 37", "37 52 2", "39 0 15"));
+        Map<Range<Long>, Range<Long>> fertilizerToWater = range(ImmutableList.of(
+                "49 53 8", "0 11 42", "42 0 7", "57 7 4"));
+        Map<Range<Long>, Range<Long>> waterToLight = range(ImmutableList.of(
+                "88 18 7", "18 25 70"));
+        Map<Range<Long>, Range<Long>> lightToTemperature = range(ImmutableList.of(
+                "45 77 23", "81 45 19", "68 64 13"));
+        Map<Range<Long>, Range<Long>> temperatureToHumidity = range(ImmutableList.of(
+                "0 69 1", "1 0 69"));
+        Map<Range<Long>, Range<Long>> humidityToLocation = range(ImmutableList.of(
+                "60 56 37", "56 93 4"));
+
+        long actual = problem.solveB(
+                seeds,
+                seedToSoil,
+                soilToFertilizer,
+                fertilizerToWater,
+                waterToLight,
+                lightToTemperature,
+                temperatureToHumidity,
+                humidityToLocation);
+        assertThat(actual).isEqualTo(46);
+    }
+
+    @Test
+    public void testGetB_noOverlaps() {
+        RangeSet<Long> seeds = seeds("364807853 408612163");
+        Map<Range<Long>, Range<Long>> seedToSoil = range(ImmutableList.of("2069473506 3732587455 1483883"));
+        RangeSet<Long> actual = problem.getB(seeds, seedToSoil);
+        RangeSet<Long> expected = TreeRangeSet.create(ImmutableSet.of(
+                Range.closedOpen(364807853L, 364807853L + 408612163L)));
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testGetB_seedsCompletelyEnclosesOther() {
+        RangeSet<Long> seeds = seeds("364807853 408612163");
+        Map<Range<Long>, Range<Long>> seedToSoil = range(ImmutableList.of("0 568494408 18343754"));
+        RangeSet<Long> actual = problem.getB(seeds, seedToSoil);
+        RangeSet<Long> expected = TreeRangeSet.create(ImmutableSet.of(
+                Range.closedOpen(364807853L, 568494408L),
+                Range.closedOpen(0L, 18343754L),
+                Range.closedOpen(586838162L, 773420016L)));
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testGetB_otherCompletelyEnclosesSeeds() {
+        RangeSet<Long> seeds = seeds("3692780593 17215731");
+        Map<Range<Long>, Range<Long>> seedToSoil = range(ImmutableList.of("605476380 3544150009 188437446"));
+        RangeSet<Long> actual = problem.getB(seeds, seedToSoil);
+        RangeSet<Long> expected = TreeRangeSet.create(ImmutableSet.of(
+                Range.closedOpen(3692780593L - 2938673629L, 3709996324L - 2938673629L)));
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testGetB_seedsLeftOffset() {
+        RangeSet<Long> seeds = seeds("364807853 408612163");
+        Map<Range<Long>, Range<Long>> seedToSoil = range(ImmutableList.of("3100147259 762191092 135543997"));
+        RangeSet<Long> actual = problem.getB(seeds, seedToSoil);
+        RangeSet<Long> expected = TreeRangeSet.create(ImmutableSet.of(
+                Range.closedOpen(364807853L, 762191092L),
+                Range.closedOpen(762191092L + 2337956167L, 773420016L + 2337956167L)));
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testGetB_seedsRightOffset() {
+        RangeSet<Long> seeds = seeds("2593569946 345762334");
+        Map<Range<Long>, Range<Long>> seedToSoil = range(ImmutableList.of("2821869347 2355540461 278277912"));
+        RangeSet<Long> actual = problem.getB(seeds, seedToSoil);
+        RangeSet<Long> expected = TreeRangeSet.create(ImmutableSet.of(
+                Range.closedOpen(2593569946L + 466328886L, 2633818373L + 466328886L),
+                Range.closedOpen(2633818373L, 2939332280L)));
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testGetB_seeds_rightOffset_seedsEnclosesOther() {
+        RangeSet<Long> seeds = seeds("364807853 408612163");
+        Map<Range<Long>, Range<Long>> seedToSoil = range(ImmutableList.of(
+                "391285622 257757572 195552540",
+                "335002083 512210869 56283539"));
+        RangeSet<Long> actual = problem.getB(seeds, seedToSoil);
+        RangeSet<Long> expected = TreeRangeSet.create(ImmutableSet.of(
+                Range.closedOpen(364807853L + 133528050L, 453310112L + 133528050L),
+                Range.closedOpen(453310112L, 512210869L),
+                Range.closedOpen(512210869L - 177208786L, 568494408L - 177208786L),
+                Range.closedOpen(568494408L, 773420016L)));
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private RangeSet<Long> seeds(String str) {
+        List<Long> tokens = Arrays.stream(str.split(" "))
+                .mapToLong(Long::parseLong)
+                .boxed()
+                .collect(Collectors.toList());
+        RangeSet<Long> result = TreeRangeSet.create();
+        for (int i = 0; i < tokens.size(); i += 2) {
+            result.add(Range.closedOpen(tokens.get(i), tokens.get(i) + tokens.get(i + 1)));
+        }
+        return result;
+    }
+
+    private Map<Range<Long>, Range<Long>> range(List<String> strs) {
+        Map<Range<Long>, Range<Long>> result = new HashMap<>();
+        for (String str : strs) {
+            List<Long> tokens = Arrays.stream(str.split(" "))
+                    .mapToLong(Long::parseLong)
+                    .boxed()
+                    .collect(Collectors.toList());
+            long diff = tokens.get(2);
+            long srcStart = tokens.get(1);
+            long dstStart = tokens.get(0);
+            result.put(
+                    Range.closedOpen(srcStart, srcStart + diff),
+                    Range.closedOpen(dstStart, dstStart + diff));
+        }
+        return result;
     }
 
 }
