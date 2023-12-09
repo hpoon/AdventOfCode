d64dfdb738a7883f73d01dfa88485b9202099174
hp <5841557+hpoon@users.noreply.github.com>
2023-12-09 00:33:32 -0500

    Completed problem 2023 day 9

--- src/main/java/com/aoc/y2023/ProblemDay9.java
+++ src/main/java/com/aoc/y2023/ProblemDay9.java
@@ -1,17 +1,77 @@
 package com.aoc.y2023;
 
 import com.aoc.ProblemDay;
+import com.google.common.annotations.VisibleForTesting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProblemDay9 extends ProblemDay<Integer, Integer> {
 
     @Override
     public Integer solveA() {
-        return 0;
+        List<List<Integer>> numsOfNums = parse();
+        return solve(numsOfNums, true);
     }
 
     @Override
     public Integer solveB() {
-        return 0;
+        List<List<Integer>> numsOfNums = parse();
+        return solve(numsOfNums, false);
+    }
+
+    private int solve(List<List<Integer>> numsOfNums, boolean forward) {
+        int sum = 0;
+        for (List<Integer> nums : numsOfNums) {
+            sum += solveOne(nums, forward);
+        }
+        return sum;
+    }
+
+    @VisibleForTesting
+    protected int solveOne(List<Integer> nums, boolean forward) {
+        List<List<Integer>> results = calculateDiffs(nums);
+        results.get(results.size() - 1).add(forward ? results.get(results.size() - 1).size() : 0, 0);
+        for (int i = results.size() - 2; i >= 0; i--) {
+            List<Integer> current = results.get(i);
+            List<Integer> lower = results.get(i+1);
+            int next = forward
+                    ? current.get(current.size() - 1) + lower.get(lower.size() - 1)
+                    : current.get(0) - lower.get(0);
+            if (i == 0) {
+                return next;
+            } else {
+                current.add(forward ? current.size() : 0, next);
+            }
+        }
+        throw new RuntimeException("Shouldn't happen");
+    }
+
+    private static List<List<Integer>> calculateDiffs(List<Integer> nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        results.add(nums);
+        while (results.get(results.size() - 1).stream().anyMatch(i -> i != 0)) {
+            List<Integer> last = results.get(results.size() - 1);
+            List<Integer> next = new ArrayList<>();
+            for (int i = 1; i < last.size(); i++) {
+                int diff = last.get(i) - last.get(i-1);
+                next.add(diff);
+            }
+            results.add(next);
+        }
+        return results;
+    }
+
+    private List<List<Integer>> parse() {
+        List<List<Integer>> numsOfNums = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            numsOfNums.add(Arrays.stream(scanner.nextLine().split(" "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList()));
+        }
+        return numsOfNums;
     }
 
 }
--- src/main/test/java/com/aoc/y2023/ProblemDay9Test.java
+++ src/main/test/java/com/aoc/y2023/ProblemDay9Test.java
@@ -1,8 +1,11 @@
 package com.aoc.y2023;
 
 import com.aoc.ProblemDayTest;
+import com.google.common.collect.ImmutableList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -18,13 +21,39 @@ public class ProblemDay9Test implements ProblemDayTest {
     @Test
     @Override
     public void testSolveA() {
-        assertThat(problem.solveA()).isEqualTo(0);
+        assertThat(problem.solveA()).isEqualTo(2105961943);
     }
 
     @Test
     @Override
     public void testSolveB() {
-        assertThat(problem.solveB()).isEqualTo(0);
+        assertThat(problem.solveB()).isEqualTo(1019);
+    }
+
+    @Test
+    public void testSolveOneForward() {
+        List<Integer> nums = ImmutableList.<Integer>builder()
+                .add(10)
+                .add(13)
+                .add(16)
+                .add(21)
+                .add(30)
+                .add(45)
+                .build();
+        assertThat(problem.solveOne(nums, true)).isEqualTo(68);
+    }
+
+    @Test
+    public void testSolveOneBackward() {
+        List<Integer> nums = ImmutableList.<Integer>builder()
+                .add(10)
+                .add(13)
+                .add(16)
+                .add(21)
+                .add(30)
+                .add(45)
+                .build();
+        assertThat(problem.solveOne(nums, false)).isEqualTo(5);
     }
 
 }
