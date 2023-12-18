f8a281ff449f0d77243b55f528e9fe455375fa5d
Henry Poon <henrpoon@amazon.com>
2023-12-17 21:46:34 -0500

    Completed problem 2023 day 12

--- src/main/java/com/aoc/y2023/ProblemDay12.java
+++ src/main/java/com/aoc/y2023/ProblemDay12.java
@@ -1,17 +1,108 @@
 package com.aoc.y2023;
 
 import com.aoc.ProblemDay;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Joiner;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
-public class ProblemDay12 extends ProblemDay<Integer, Integer> {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ProblemDay12 extends ProblemDay<Long, Long> {
 
     @Override
-    public Integer solveA() {
-        return 0;
+    public Long solveA() {
+        List<Pair<String, List<Integer>>> lines = parse();
+        long ways = 0;
+        for (Pair<String, List<Integer>> line : lines) {
+            String pattern = line.getKey();
+            List<Integer> damage = line.getValue();
+            ways += solve(pattern, damage, new HashMap<>());
+        }
+        return ways;
     }
 
     @Override
-    public Integer solveB() {
-        return 0;
+    public Long solveB() {
+        List<Pair<String, List<Integer>>> lines = parse();
+        long ways = 0;
+        for (Pair<String, List<Integer>> line : lines) {
+            String pattern = Joiner.on("?").join(
+                    IntStream.range(0, 5)
+                            .boxed()
+                            .map(i -> line.getKey())
+                            .collect(Collectors.toList()));
+            List<Integer> damage = IntStream.range(0, 5)
+                    .boxed()
+                    .flatMap(i -> line.getValue().stream())
+                    .collect(Collectors.toList());
+            ways += solve(pattern, damage, new HashMap<>());
+        }
+        return ways;
+    }
+
+    @VisibleForTesting
+    protected long solve(String remainder, List<Integer> damage, Map<Memoization, Long> memo) {
+        Memoization m = new Memoization(remainder, damage);
+        if (memo.containsKey(m)) {
+            return memo.get(m);
+        }
+        if (StringUtils.isBlank(remainder)) {
+            return damage.isEmpty() ? 1 : 0;
+        }
+        if (damage.isEmpty()) {
+            return remainder.matches("^[.?]+$") ? 1 : 0;
+        }
+
+        long ways = 0;
+        char next = remainder.charAt(0);
+        int dmg = damage.get(0);
+        if (next == '#') {
+            if (remainder.length() >= dmg) {
+                if (remainder.length() == dmg
+                        && remainder.substring(0, dmg).matches("^[?#]+$")) {
+                    ways = solve(remainder.substring(dmg), damage.subList(1, damage.size()), memo);
+                } else if (remainder.length() > dmg
+                        && remainder.substring(0, dmg + 1).matches(String.format("^[?#]{%d}(?:[.?][?#]*)?$", dmg))) {
+                    ways = solve(remainder.substring(dmg + 1), damage.subList(1, damage.size()), memo);
+                }
+            }
+        } else if (next == '.') {
+            ways = solve(remainder.substring(1), damage, memo);
+        } else {
+            ways = solve('.' + remainder.substring(1), damage, memo)
+                    + solve('#' + remainder.substring(1), damage, memo);
+        }
+        memo.put(m, ways);
+        return ways;
+    }
+
+    private List<Pair<String, List<Integer>>> parse() {
+        List<Pair<String, List<Integer>>> lines = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            String[] tokens = scanner.nextLine().split(" ");
+            lines.add(ImmutablePair.of(
+                    tokens[0],
+                    Arrays.stream(tokens[1].split(",")).map(Integer::parseInt).collect(Collectors.toList())
+            ));
+        }
+        return lines;
+    }
+
+    @Value
+    @EqualsAndHashCode
+    private static class Memoization {
+        String remainder;
+        List<Integer> damage;
     }
 
 }
--- src/main/test/java/com/aoc/y2023/ProblemDay12Test.java
+++ src/main/test/java/com/aoc/y2023/ProblemDay12Test.java
@@ -1,8 +1,11 @@
 package com.aoc.y2023;
 
 import com.aoc.ProblemDayTest;
+import com.google.common.collect.ImmutableList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -18,13 +21,18 @@ public class ProblemDay12Test implements ProblemDayTest {
     @Test
     @Override
     public void testSolveA() {
-        assertThat(problem.solveA()).isEqualTo(0);
+        assertThat(problem.solveA()).isEqualTo(7622);
     }
 
     @Test
     @Override
     public void testSolveB() {
-        assertThat(problem.solveB()).isEqualTo(0);
+        assertThat(problem.solveB()).isEqualTo(4964259839627L);
+    }
+
+    @Test
+    public void testSolve() {
+        assertThat(problem.solve("???.###", ImmutableList.of(1, 1, 3), new HashMap<>())).isEqualTo(1);
     }
 
 }
