3164bcf35cb86f595baaf407f8cc23f206e07b05
hp <carollui@MacBook-Pro.local>
2024-05-02 22:08:37 -0400

    Completed problem 2016 day 6

--- src/main/java/com/aoc/y2016/ProblemDay6.java
+++ src/main/java/com/aoc/y2016/ProblemDay6.java
@@ -2,16 +2,64 @@ package com.aoc.y2016;
 
 import com.aoc.ProblemDay;
 
-public class ProblemDay6 extends ProblemDay<Integer, Integer> {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProblemDay6 extends ProblemDay<String, String> {
 
     @Override
-    public Integer solveA() {
-        return 0;
+    public String solveA() {
+        List<String> strings = parse();
+        return solve(strings, true);
     }
 
     @Override
-    public Integer solveB() {
-        return 0;
+    public String solveB() {
+        List<String> strings = parse();
+        return solve(strings, false);
+    }
+
+    private String solve(List<String> strings, boolean isMax) {
+        List<String> transposed = transpose(strings);
+        StringBuilder builder = new StringBuilder();
+        for (String string : transposed) {
+            Map<Character, Integer> freqs = new HashMap<>();
+            for (int i = 0; i < string.length(); i++) {
+                freqs.put(string.charAt(i), freqs.getOrDefault(string.charAt(i), 0) + 1);
+            }
+            int limit = isMax ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            char limitChar = '_';
+            for (Map.Entry<Character, Integer> entry : freqs.entrySet()) {
+                if ((isMax && entry.getValue() > limit) || (!isMax && entry.getValue() < limit)) {
+                    limit = entry.getValue();
+                    limitChar = entry.getKey();
+                }
+            }
+            builder.append(limitChar);
+        }
+        return builder.toString();
+    }
+
+    private List<String> transpose(List<String> strings) {
+        List<String> transposed = new ArrayList<>();
+        for (int col = 0; col < strings.get(0).length(); col++) {
+            StringBuilder sb = new StringBuilder();
+            for (int row = 0; row < strings.size(); row++) {
+                sb.append(strings.get(row).charAt(col));
+            }
+            transposed.add(sb.toString());
+        }
+        return transposed;
+    }
+
+    private List<String> parse() {
+        List<String> strings = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            strings.add(scanner.nextLine());
+        }
+        return strings;
     }
 
 }
